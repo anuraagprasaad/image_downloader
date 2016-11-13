32f57b139f6fc0ed3db50d5309d6b05daa9fde7a
anuraagprasaad <anuraagprasaad@gmail.com>
2016-11-13 11:13:19 +0800

    Update img_utilities.py

--- src/img_utilities.py
+++ src/img_utilities.py
@@ -3,9 +3,11 @@ from posixpath import basename, dirname
 
 import re
 
+# remove empty lines from the urls.txt file
 def remove_empty_lines(url_list):
     return list(filter(lambda x: not re.match(r'^\s*$', x), url_list))
 
+# remove white space characters from the urls.txt file
 def remove_whitespace_character(url_list):
     return list(map(lambda string: string.strip(), url_list))
 
@@ -21,6 +23,7 @@ def get_dirname(url):
 def get_basename(url):
     return basename(urlparse(url).path)
 
+# checks if an image url is a valid url
 def is_valid_img_url(url):
         if not urlparse(url).scheme == '' and not urlparse(url).netloc == '' and has_valid_img_ext(url):
             return True
